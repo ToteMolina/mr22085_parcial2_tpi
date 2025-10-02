110f96792f655e993dc1f5909250619dde65c5d3
ToteMolina <totelimo@gmail.com>
2025-10-02 11:20:34 -0600

    modificando estructura de proyecto

--- ejercicio1/mvc/autoloader.php
+++ ejercicio1/mvc/autoloader.php
@@ -1,13 +1,12 @@
-<?php
-#funciones anonimas
-   spl_autoload_register(function($clase){
-       $ruta = "../".str_replace("\\","/",$clase).".php";
-       if(file_exists($ruta)) {
-           require_once $ruta;
-       } else {
-           die("No se pudo cargar la ruta $ruta");
-       }
-   });
+<?php 
+spl_autoload_register(function($clase){
+    $ruta ="../".str_replace("\\","/",$clase).".php";
 
+    if(file_exists($ruta)){
+        require_once($ruta);
 
+    }else{
+        die("No se pudo cargar la ruta $ruta");
+    }
+});
 ?>
---
+++ ejercicio2/Database.php
@@ -0,0 +1,23 @@
+<?php
+
+class Database {
+    private $host = "db";
+    private $dbname = "db_name";
+    private $username = "root";
+    private $password = "rootpass";
+    public $conn;
+    public function getConnection(){
+        $this->conn = null;
+        try {
+            $this->conn = new PDO("mysql:80=" . $this->host . ";dbname=" . $this->dbname, $this->username, $this->password);
+            $this->conn->exec("set names utf8");
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            echo "Conectado";
+        } catch (PDOException $exception) {
+            echo "Error en la conexión: " . $exception->getMessage();
+        }
+        return $this->conn;
+    }
+}
+
+?>
